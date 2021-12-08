5de1916d0f4f95b52e722a0209b8f8fb3492a6d9
Damian Antonowicz <kontakt@damianantonowicz.pl>
2021-12-08 17:45:11 +0100

    Added HandlersPage.

--- AirPollutionMonitor/App.xaml.cs
+++ AirPollutionMonitor/App.xaml.cs
@@ -2,6 +2,7 @@
 using Microsoft.Maui.Controls;
 using Microsoft.Maui.Controls.PlatformConfiguration.WindowsSpecific;
 using Microsoft.Maui.Controls.Xaml;
+using Microsoft.Maui.Graphics;
 using Application = Microsoft.Maui.Controls.Application;
 
 namespace AirPollutionMonitor
@@ -12,6 +13,15 @@ namespace AirPollutionMonitor
 		public App()
 		{
 			InitializeComponent();
+
+			Microsoft.Maui.Handlers.CheckBoxHandler.CheckBoxMapper.AppendToMapping(nameof(IView.Background), (handler, view) =>
+			{
+#if WINDOWS
+				handler.NativeView.Background = Colors.Red.ToNative();
+#elif __ANDROID__
+				handler.NativeView.SetBackgroundColor(Colors.Red.ToNative());
+#endif
+			});
 		}
 	}
 }
---
+++ AirPollutionMonitor/Views/HandlersPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AirPollutionMonitor.ViewModels;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+
+namespace AirPollutionMonitor
+{
+	public partial class HandlersPage : ContentPage
+	{
+        public HandlersPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
