b549565aaf6b1d6cba8bd5a2ece3b897d6911cb1
Damian Antonowicz <kontakt@damianantonowicz.pl>
2021-12-17 16:27:44 +0100

    Added ImagePage.

---
+++ AirPollutionMonitor/Views/ImagePage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using AirPollutionMonitor.Services;
+using AirPollutionMonitor.ViewModels;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+
+namespace AirPollutionMonitor
+{
+	public partial class ImagePage : ContentPage
+	{
+        public ImagePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
