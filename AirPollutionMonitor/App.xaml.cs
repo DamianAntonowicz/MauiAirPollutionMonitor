f101c42c093fb93cfde4edcab14d5081726bafa9
Damian Antonowicz <dmtn@demant.com>
2022-03-31 21:07:30 +0200

    Updated project to latest preview of .NET MAUI.

--- AirPollutionMonitor/App.xaml.cs
+++ AirPollutionMonitor/App.xaml.cs
@@ -1,27 +1,9 @@
-﻿using Microsoft.Maui;
-using Microsoft.Maui.Controls;
-using Microsoft.Maui.Controls.PlatformConfiguration.WindowsSpecific;
-using Microsoft.Maui.Controls.Xaml;
-using Microsoft.Maui.Graphics;
-using Application = Microsoft.Maui.Controls.Application;
+﻿namespace AirPollutionMonitor;
 
-namespace AirPollutionMonitor
+public partial class App : Application
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class App : Application
+	public App()
 	{
-		public App()
-		{
-			InitializeComponent();
-
-			Microsoft.Maui.Handlers.CheckBoxHandler.CheckBoxMapper.AppendToMapping(nameof(IView.Background), (handler, view) =>
-			{
-#if WINDOWS
-				handler.NativeView.Background = Colors.Pink.ToNative();
-#elif __ANDROID__
-				handler.NativeView.SetBackgroundColor(Colors.Red.ToNative());
-#endif
-			});
-		}
+		InitializeComponent();
 	}
 }
