f2f1182a19222e75211309e4fd7dc8867a285294
Damian Antonowicz <dmtn@demant.com>
2022-03-31 22:11:26 +0200

    Updated handlers sample.

--- AirPollutionMonitor/App.xaml.cs
+++ AirPollutionMonitor/App.xaml.cs
@@ -5,5 +5,14 @@ public partial class App : Application
 	public App()
 	{
 		InitializeComponent();
+
+		Microsoft.Maui.Handlers.CheckBoxHandler.Mapper.AppendToMapping(nameof(IView.Background), (handler, view) =>
+		{
+#if WINDOWS
+			handler.PlatformView.Background = new Microsoft.UI.Xaml.Media.SolidColorBrush(Microsoft.UI.Colors.Red);
+#elif __ANDROID__
+			handler.PlatformView.SetBackgroundColor(Android.Graphics.Color.Green);
+#endif
+		});
 	}
 }
