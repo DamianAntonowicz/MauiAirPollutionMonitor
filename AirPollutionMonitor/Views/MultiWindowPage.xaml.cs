79e13c91e83acab3c28b3449fb2d15744698818e
Damian Antonowicz <dmtn@demant.com>
2022-04-13 19:23:56 +0200

    Added multi window sample.

---
+++ AirPollutionMonitor/Views/MultiWindowPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using AirPollutionMonitor.Services;
+using AirPollutionMonitor.ViewModels;
+using Microsoft.Maui.Controls;
+
+namespace AirPollutionMonitor
+{
+	public partial class MultiWindowPage : ContentPage
+	{
+        public MultiWindowPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var secondWindow = new Window
+            {
+                Page = new ImagePage()
+            };
+
+            Application.Current.OpenWindow(secondWindow);
+        }
+    }
+}
