3910d5047d6f047eeb24ecf35d51a71f1975e578
Damian Antonowicz <dmtn@demant.com>
2022-04-13 20:17:41 +0200

    Added shadow example.

---
+++ AirPollutionMonitor/Views/ShadowsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AirPollutionMonitor.Services;
+using AirPollutionMonitor.ViewModels;
+using Microsoft.Maui.Controls;
+
+namespace AirPollutionMonitor
+{
+	public partial class ShadowsPage : ContentPage
+	{
+        public ShadowsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
