e04c159a29224d52d37613f97c64f8393be1fd44
Damian Antonowicz <dmtn@demant.com>
2022-04-13 21:49:33 +0200

    Added border sample.

---
+++ AirPollutionMonitor/Views/BorderPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AirPollutionMonitor.Services;
+using AirPollutionMonitor.ViewModels;
+using Microsoft.Maui.Controls;
+
+namespace AirPollutionMonitor
+{
+	public partial class BorderPage : ContentPage
+	{
+        public BorderPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
